c96b5c556722c5b10231aa1dd71e2f7bbaef2fda
720360 <nikkimeyer.07@gmail.com>
2016-12-13 22:19:31 -0600

    Add files via upload

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,10 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { medium: "200x>", thumb: "100x100>" }, :default_url => "missing.jpg"
-	# validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
-	# validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
-	# do_not_validate_attachment_file_type
-	# validates_attachment_content_type :image, :content_type => [/png\Z/, /jpe?g\Z/, /gif\Z/]
-	# validates :image, attachment_presence: true
-	# validates_with AttachmentPresenceValidator, attributes: :image
-	# validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes
-	# validates_attachment :image, presence: true, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
-	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "No-image-found.jpg"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+validates :name, :description, :price, presence: true
+validates :price, numericality: {greater_than:0}
+
+belongs_to :user
 end
